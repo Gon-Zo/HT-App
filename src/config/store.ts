f4877ca4512b96d8b55109da61a9cb62307670be
gonzo <m165252@daum.net>
2021-02-13 09:59:24 +0900

    refactor: #1 promise middleware 세팅
    - promise middleware config file 구성

--- src/config/store.ts
+++ src/config/store.ts
@@ -3,7 +3,11 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../shared/reducer';
-import promiseMiddleware from 'redux-promise-middleware';
+import {createPromise} from 'redux-promise-middleware';
+
+const promiseMiddleware = createPromise({
+    promiseTypeSuffixes: ['REQUEST', 'SUCCESS', 'FAILURE'],
+});
 
 const defaultMiddlewares = [
     thunkMiddleware,
