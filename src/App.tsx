7fa1ba2846462ce1fad383ef543c9b5e33f9f18e
‘gonzo’ <pnw123456@icloud.com>
2021-02-13 10:09:33 +0900

    style: 기타 이름 변경
    - diplay name 변경
    - constants.ts 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Count from "../src/module/count/count";
 import {Provider} from 'react-redux';
 import store from './config/store';
-
 const App = () => {
     return (
         <Provider store={store}>
