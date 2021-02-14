4e1ad147ac78d72b8db4958b99c633e02c303b71
gonzo <m165252@daum.net>
2021-02-14 20:05:35 +0900

    feature: #2 price redux
    - price redux 구성
    - xml -> json 으로 변경

--- src/module/price/price.reducer.ts
+++ src/module/price/price.reducer.ts
@@ -1,16 +1,6 @@
 import {PriceInterface} from './price.interface';
 import {FAILURE, REQUEST, SUCCESS} from '../../shared/utils/action-utils';
 import axios from 'axios';
-const parser = require('fast-xml-parser');
-const he = require('he');
-
-const options = {
-    ignoreAttributes: false,
-    ignoreNameSpace: false,
-    parseNodeValue: true,
-    parseAttributeValue: true,
-    trimValues: true,
-};
 
 const ACTION_TYPES = {
     FETCH_HOMES_PRICE_DATA: 'price/FETCH_HOMES_PRICE_DATA',
@@ -30,7 +20,7 @@ export default (state = initialState, action: any): PriceState => {
                 priceList: {
                     load: false,
                     error: null,
-                    data: action.payload,
+                    data: action.payload.data.response.body,
                 },
             };
         case REQUEST(ACTION_TYPES.FETCH_HOMES_PRICE_DATA):
@@ -57,31 +47,11 @@ export default (state = initialState, action: any): PriceState => {
 }
 
 export const getByTest = () => {
-    const reqUri = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?ServiceKey=0pAYHFBPkd%2BFYQMVlBZnPxCWsbgGCspccauAOqAHVZhVpLec3iEGOFMTNTLWE%2F%2BXny%2B1dEzLcZhAwqvLxJEYFA%3D%3D&pageNo=1&numOfRows=10&LAWD_CD=11110&DEAL_YMD=201512'
-    return async (dispatch : any , getState : any) =>{
-        dispatch({type : ACTION_TYPES.FETCH_HOMES_PRICE_DATA , payload : axios.get(reqUri)})
-    }
-    // const requestUri = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev';
-    // const pageNo = 1;
-    // const numOfRows = 10;
-    // const lawdCd = 11110;
-    // const dealYmd = 201512;
-    // const params = {
-    //     ServiceKey: '0pAYHFBPkd%2BFYQMVlBZnPxCWsbgGCspccauAOqAHVZhVpLec3iEGOFMTNTLWE%2F%2BXny%2B1dEzLcZhAwqvLxJEYFA%3D%3D',
-    //     pageNo: pageNo,
-    //     numOfRows: numOfRows,
-    //     'LAWD_CD': lawdCd,
-    //     'DEAL_YMD': dealYmd,
-    // };
-    // return async (dispatch: any, getState: any) => {
-    //     await dispatch({
-    //         type: ACTION_TYPES.FETCH_HOMES_PRICE_DATA,
-    //         payload: axios.get(requestUri, {
-    //             params: params, headers: {
-    //                 'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    //                 'Accept': '*/*',
-    //             },
-    //         }),
-    //     });
-    // };
+    const serviceKey = '0pAYHFBPkd%2BFYQMVlBZnPxCWsbgGCspccauAOqAHVZhVpLec3iEGOFMTNTLWE%2F%2BXny%2B1dEzLcZhAwqvLxJEYFA%3D%3D'
+    const monthDay = '201512'
+    const localNumber = '11110'
+    const reqUri = `http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?ServiceKey=${serviceKey}&pageNo=1&numOfRows=10&LAWD_CD=${localNumber}&DEAL_YMD=${monthDay}`;
+    return async (dispatch: any, getState: any) => {
+        dispatch({type: ACTION_TYPES.FETCH_HOMES_PRICE_DATA, payload: axios.get(reqUri)});
+    };
 };
