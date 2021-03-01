c96db79d1416aa4dc317982b0ec81e046e22d07d
gonzo <m165252@daum.net>
2021-03-01 22:51:13 +0900

    feat: #3 차트 라이브러리 사용
    - 바 차트
    - 라인 차트
    - 파이 차트
    - 스텍 차트
    
    Closes #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from './config/store';
-import Count from './module/count/count';
+import {
+    SafeAreaView,
+    Text,
+    View,
+    StyleSheet,
+    ScrollView,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: 10,
+    },
+    header: {
+        textAlign: 'center',
+        fontSize: 18,
+        padding: 16,
+        marginTop: 16,
+    },
+});
 
 const App = () => {
     return (
         <Provider store={store}>
-            <Count/>
+            <SafeAreaView style={{flex: 1}}>
+                <ScrollView>
+                    <View style={styles.container}>
+                        <View>
+                            <Text>TEST</Text>
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         </Provider>
     );
 };
