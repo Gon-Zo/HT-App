42772af0b9846afa3e1e3d67b8f584260b103301
gonzo <m165252@daum.net>
2021-03-15 21:36:51 +0900

    feat: main view develop #2
    - layout 구성하기

---
+++ src/module/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Text, View} from 'react-native';
+import Layout from '../shared/Layout';
+
+const Main = (props: any) => {
+    return (
+        <Layout>
+            <View>
+                <Text>MainView</Text>
+            </View>
+        </Layout>
+    );
+};
+
+export default Main
