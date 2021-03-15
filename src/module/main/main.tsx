5791daba519fd4b0a814e665898023d965d2ed69
gonzo <m165252@daum.net>
2021-03-15 22:02:19 +0900

    feat: main view develop #2
    - 메인 차트 바 컨포넌트

---
+++ src/module/main/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../shared/Layout';
+import MainBarChart from './main-bar-chart';
+
+const Main = (props: any) => {
+    return (
+        <Layout>
+            <MainBarChart/>
+        </Layout>
+    );
+};
+
+export default Main
