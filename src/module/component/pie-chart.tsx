92316394f91d7308fd424d2afd9c9f1c250dd3ea
‘gonzo’ <pnw123456@icloud.com>
2021-03-16 22:16:42 +0900

    feat: main view develop #2
    - 메인 파이 차트 컨포넌트

--- src/module/component/pie-chart.tsx
+++ src/module/component/pie-chart.tsx
@@ -2,39 +2,6 @@ import {Dimensions, Text} from 'react-native';
 import {PieChart} from 'react-native-chart-kit';
 import React from 'react';
 
-/*
-                    [
-                    {
-                        name: 'Seoul',
-                        population: 21500000,
-                        color: 'rgba(131, 167, 234, 1)',
-                        legendFontColor: '#7F7F7F',
-                        legendFontSize: 15,
-                    },
-                    {
-                        name: 'Toronto',
-                        population: 2800000,
-                        color: '#F00',
-                        legendFontColor: '#7F7F7F',
-                        legendFontSize: 15,
-                    },
-                    {
-                        name: 'New York',
-                        population: 8538000,
-                        color: '#ffffff',
-                        legendFontColor: '#7F7F7F',
-                        legendFontSize: 15,
-                    },
-                    {
-                        name: 'Moscow',
-                        population: 11920000,
-                        color: 'rgb(0, 0, 255)',
-                        legendFontColor: '#7F7F7F',
-                        legendFontSize: 15,
-                    },
-                ]
- */
-
 const HTPieChart = (props: any) => {
     return (
         <React.Fragment>
