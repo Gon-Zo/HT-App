ac40d463bec07aedb9adca6804ebed4c6e8484b8
gonzo <m165252@daum.net>
2021-03-16 22:16:42 +0900

    feat: main view develop #2
    - 메인 파이 차트 컨포넌트

--- src/module/main/main.tsx
+++ src/module/main/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../shared/Layout';
 import MainBarChart from './main-bar-chart';
+import MainPieChart from './main-pie-chart';
 
 const Main = (props: any) => {
     return (
         <Layout>
             <MainBarChart/>
+            <MainPieChart/>
         </Layout>
     );
 };
