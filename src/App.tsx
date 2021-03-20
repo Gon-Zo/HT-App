22cc65e5351599e5804fbcc0966540c0512391bf
‘gonzo’ <pnw123456@icloud.com>
2021-03-20 10:50:47 +0900

    feat: React Navigation #5
    - react navigation 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './config/store';
 import Main from './module/main/main';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
     return (
         <Provider store={store}>
-            <Main/>
+            <NavigationContainer>
+                <Main/>
+            </NavigationContainer>
         </Provider>
     );
 };
