30bd425c6d18f10d7e8c594961f9d38672548560
‘gonzo’ <pnw123456@icloud.com>
2021-03-20 11:07:33 +0900

    feat: React Navigation #5
    - react navigation stack 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from './config/store';
-import Main from './module/main/main';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Main from './module/main/main';
+
+const Stack = createStackNavigator();
 
 const App = () => {
     return (
         <Provider store={store}>
             <NavigationContainer>
-                <Main/>
+                <Stack.Navigator>
+                    <Stack.Screen name="Main" component={Main}/>
+                </Stack.Navigator>
             </NavigationContainer>
         </Provider>
     );
