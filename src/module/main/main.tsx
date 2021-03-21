17cd71a92b6721dd5ff4b14b8a72deb2f6157d7a
‘gonzo’ <pnw123456@icloud.com>
2021-03-21 21:52:18 +0900

    feat: Custom Header 구성 #5
    - 커스텀 헤더 구성
    - 공통 컴포넌트 구성

--- src/module/main/main.tsx
+++ src/module/main/main.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MainBarChart from './main-bar-chart';
 import MainPieChart from './main-pie-chart';
 import MainProgressChart from './main-progress-chart';
 import ScrollerLayout from '../../shared/assest/scroller-layout';
-import {Button} from 'react-native';
 import {NavigationProp} from '@react-navigation/native';
+import {LogoImage, MAIN_HEADER_OPTIONS, RightButton} from '../component/ht-component';
 
 interface IMainProps {
     navigation: NavigationProp<any>
 }
 
+
 const Main = (props: IMainProps) => {
 
     const {navigation} = props;
 
+    const _goToSearchView = () => navigation.navigate('Search');
+
+    useEffect(() => {
+        navigation.setOptions(MAIN_HEADER_OPTIONS(_goToSearchView));
+    }, [navigation]);
+
     return (
         <ScrollerLayout>
-            <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Search')}/>
             <MainBarChart/>
             <MainPieChart/>
             <MainProgressChart/>
---
+++ src/shared/assest/ht-navigation.interface.ts
@@ -0,0 +1,8 @@
+import {NavigationProp} from '@react-navigation/native';
+
+export interface IHtNavigation {
+}
+
+export interface IRightButton {
+    onPress: Function
+}
