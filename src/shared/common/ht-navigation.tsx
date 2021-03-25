bf57bd70fd655e1b2e903de5b8e51cae1e97651f
gonzo <m165252@daum.net>
2021-03-25 22:24:14 +0900

    feat: React Navigation #5
    - Tab 네비게이션 구성
    - Stack 네비게이션 에서 Tab hidden

--- src/shared/common/ht-navigation.tsx
+++ src/shared/common/ht-navigation.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import {IHtNavigation} from './interface/common.interface';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import StackMainNavigation from './stack-main-navigation';
 import {Text, View} from 'react-native';
+import ScrollerLayout from './scroller-layout';
 
 const Tab = createBottomTabNavigator();
 
-const SettingsScreen = () =>{
+const TempView = () => {
     return (
-        <View>
-            <Text>TEST</Text>
-        </View>
-    )
-}
+        <ScrollerLayout>
+            <View>
+                <Text>TEST</Text>
+            </View>
+        </ScrollerLayout>
+    );
+};
 
 const HtNavigation = (props: IHtNavigation) => {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Home" component={StackMainNavigation} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
+                <Tab.Screen name="홈" component={StackMainNavigation}/>
+                <Tab.Screen name="지역" component={TempView}/>
+                <Tab.Screen name="내주변" component={TempView}/>
+                <Tab.Screen name="My 세팅" component={TempView}/>
             </Tab.Navigator>
         </NavigationContainer>
     );
