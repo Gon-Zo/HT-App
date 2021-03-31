ac3836a548ff357323a3457fe7b1d6a044c21ce6
‘gonzo’ <pnw123456@icloud.com>
2021-03-31 20:27:45 +0900

    feat: 커스텀 탭 UI #8
    - 커스텀 탭 UI 개발

--- src/module/component/interface/component.interface.ts
+++ src/module/component/interface/component.interface.ts
@@ -1,3 +1,13 @@
+import {NavigationHelpers, ParamListBase, TabNavigationState} from '@react-navigation/native';
+import {BottomTabDescriptorMap, BottomTabNavigationEventMap} from '@react-navigation/bottom-tabs/src/types';
+
 export interface ITextBox {
     text: string
 }
+
+export interface ICustomTabProps {
+    state: TabNavigationState<ParamListBase>;
+    descriptors: BottomTabDescriptorMap;
+    navigation: NavigationHelpers<ParamListBase, BottomTabNavigationEventMap>;
+}
+
