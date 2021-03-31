a04f0c1458027ee09a97bad40f6bdbf34dd1ecf7
‘gonzo’ <pnw123456@icloud.com>
2021-03-31 20:17:13 +0900

    feat: 검색 스크린 #7
    - 검색 스크린 인풋 박스 개발

--- src/module/search/search.tsx
+++ src/module/search/search.tsx
@@ -1,17 +1,42 @@
-import React, {useEffect} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import ScrollerLayout from '../../shared/common/scroller-layout';
 import {NavigationProp} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {ParamListBase} from '@react-navigation/routers';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 interface ISearchProps {
     navigation: NavigationProp<ParamListBase>
 }
 
+interface ILeftButtonProps {
+    navigation: NavigationProp<ParamListBase>
+}
+
+const LeftButton = (props: ILeftButtonProps) => {
+    const {navigation} = props;
+    return (
+        <TouchableOpacity
+            activeOpacity={1}
+            style={styles.leftBtn}
+            onPress={() => {
+                // @ts-ignore
+                navigation.dangerouslyGetParent().setOptions({
+                    tabBarVisible: true,
+                });
+                navigation.goBack();
+            }}>
+            <Icon name={'angle-left'} size={30} color={'#000'}/>
+        </TouchableOpacity>
+    );
+};
+
 const Search = (props: ISearchProps) => {
 
     const {navigation} = props;
+
+    const [text, setText] = useState<string>('');
 
     useEffect(() => {
         // @ts-ignore
@@ -21,43 +46,44 @@ const Search = (props: ISearchProps) => {
 
         navigation.setOptions(
             {
-                headerStyle: {
-                    backgroundColor: '#f4511e',
-                },
-                headerTintColor: '#fff',
                 headerTitle: '검색',
                 headerTitleStyle: {
                     fontWeight: 'bold',
                 },
-                headerLeft: () => (
-                    <TouchableOpacity
-                        activeOpacity={1}
-                        style={{
-                            left: 10,
-                        }}
-                        onPress={() => {
-                            // @ts-ignore
-                            navigation.dangerouslyGetParent().setOptions({
-                                tabBarVisible: true,
-                            });
-                            navigation.goBack();
-                        }}>
-                        <Icon name={'angle-left'} size={30} color={'#000'}/>
-                    </TouchableOpacity>
-                ),
+                headerLeft: () => (<LeftButton navigation={navigation}/>),
             });
     }, []);
 
     return (
         <ScrollerLayout>
-            <View>
-                <Text>
-                    Search Layout
-                </Text>
+            <View style={{
+                width : 400 ,
+                backgroundColor : "#f00",
+                justifyContent : "space-between"
+            }}>
+                <TextInput
+                    style={styles.input}
+                    value={text}
+                    onChangeText={setText}
+                    placeholder="useless placeholder"
+                />
+                <FontAwesome5Icon name={"search"} size={24}/>
             </View>
         </ScrollerLayout>
     );
 
 };
+
+const styles = StyleSheet.create({
+    leftBtn: {
+        left: 10,
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        width: 300,
+    },
+});
 
 export default React.memo(Search);
