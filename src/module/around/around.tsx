f0b485cfaa4fefdcd75b243460dceee14cc248f2
‘gonzo’ <pnw123456@icloud.com>
2021-04-11 22:09:14 +0900

    feat: 내 주변 스크린 #9
    - 커스텀 헤더 적용 / 필터 적용 스타일 적용

--- src/module/around/around.tsx
+++ src/module/around/around.tsx
@@ -1,21 +1,79 @@
 import React, {useEffect} from 'react';
 import ScrollerLayout from '../../shared/common/scroller-layout';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {IAroundProps} from './around.interface';
 import {SearchButton} from '../component/ht-button';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Around = (props: IAroundProps) => {
 
-    const {navigation} = props
+    const {navigation} = props;
 
     useEffect(() => {
-       navigation.setOptions({
-           headerTitle : "내주변",
-           headerTitleStyle: {
-               fontWeight: 'bold',
-           },
-           headerRight: (() => <SearchButton navigation={navigation}/>),
-       })
+        navigation.setOptions({
+            headerTitle: '내주변',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+            headerRight: (() => <SearchButton navigation={navigation}/>),
+            header: () => {
+                return (
+                    <View style={{
+                        backgroundColor : "#fff"
+                    }}>
+                        <View style={{
+                            height: 50,
+                            backgroundColor: '#f00',
+                            marginTop: 40,
+                            flexDirection: 'row',
+                        }}>
+                            <Text>This is CustomHeader</Text>
+                        </View>
+                        <View style={{
+                            flexDirection: 'row',
+                            height: 50,
+                        }}>
+                            <TouchableOpacity style={{
+                                flexDirection: 'row',
+                                flex: 1,
+                                height: 40,
+                                alignItems: 'center',
+                                marginLeft: 20,
+                                marginTop: 5,
+                                marginRight: 5,
+                                borderWidth : .7,
+                                borderRadius : 5,
+                                borderColor : 'rgba( 0 , 0, 0, 0.7)'
+                            }}>
+                                <Icon style={{
+                                    paddingLeft : 12
+                                }} name="map-marker" size={18}/>
+                                <Text style={{
+                                    paddingLeft : 5,
+                                    fontSize : 16,
+                                    fontWeight : '500'
+                                }}>
+                                    서초/신사/방배
+                                </Text>
+                            </TouchableOpacity>
+                            <View style={{
+                                flexDirection: 'column',
+                                backgroundColor: 'gray',
+                                flex: 1,
+                                height: 40,
+                                marginRight: 20,
+                                marginTop: 5,
+                                marginLeft: 5,
+                            }}>
+                                <Text>
+                                    TEST
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                );
+            },
+        });
     }, []);
 
     return (
