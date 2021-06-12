a96eaf42bdbfd65c570adc2a7a667c255b7fe04f
gonzo <m165252@daum.net>
2021-06-12 21:40:07 +0900

    feat:fontawesome add
    - fontawesome update

--- src/entity/home/index.tsx
+++ src/entity/home/index.tsx
@@ -2,6 +2,9 @@ import React, {memo} from 'react'
 import ScrollLayout from "../../shared/component/scroll-layout";
 import {Button, Text, View} from "react-native";
 
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 import {NavigationProp} from '@react-navigation/native';
 
 type HomeProps = {
@@ -18,6 +21,9 @@ const Home = (props: HomeProps) => {
                 <Text>
                     Hom Screen
                 </Text>
+
+                <FontAwesomeIcon icon={faCoffee} />
+
                 <Button title={"onClick"} onPress={() => {
                     // Modal Screen
                     navigation.navigate("Modal")
