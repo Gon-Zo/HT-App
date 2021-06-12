c96f56def4e5a1252bc7a5bcfd91178266db71b2
‘gonzo’ <pnw123456@icloud.com>
2021-06-12 11:07:36 +0900

    feat: search screen
    - search index.tsx add
    - root stack nav add

--- src/shared/navigation/root-navigation.tsx
+++ src/shared/navigation/root-navigation.tsx
@@ -1,25 +1,17 @@
 import React, {memo} from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
-import {Text, View} from "react-native";
 import {NavigationContainer} from '@react-navigation/native'
 import TapNavigation from "./tap-navigation";
+import Search from "../../entity/search";
 
 const RootStack = createStackNavigator()
-
-const MainModal = () => {
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Home Screen</Text>
-        </View>
-    );
-}
 
 const RootStackNavigation = () => {
     return (
         <NavigationContainer>
-            <RootStack.Navigator>
-                <RootStack.Screen name={"Tab"} component={TapNavigation}/>
-                <RootStack.Screen name={"Modal"} component={MainModal}/>
+            <RootStack.Navigator mode={"modal"} headerMode={"none"}>
+                <RootStack.Screen name={"Tap"} component={TapNavigation}/>
+                <RootStack.Screen name={"Modal"} component={Search}/>
             </RootStack.Navigator>
         </NavigationContainer>
     )
