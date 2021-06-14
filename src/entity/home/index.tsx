e584a9e94c6500e3e51f5eb46812fe6867eede16
‘gonzo’ <pnw123456@icloud.com>
2021-06-14 21:05:52 +0900

    feat: bar-chart.tsx
    - bar-chart 기본 컴포넌트 개발

--- src/entity/home/index.tsx
+++ src/entity/home/index.tsx
@@ -3,10 +3,7 @@ import ScrollLayout from "../../shared/component/scroll-layout";
 import {Button, Text, View} from "react-native";
 
 import {NavigationProp} from '@react-navigation/native';
-
-// import {LineChart} from 'react-native-charts-wrapper'
-
-import {VictoryBar} from "victory-native";
+import BarChart from "../../shared/chart/bar-chart";
 
 type HomeProps = {
     navigation: NavigationProp<any>
@@ -19,20 +16,11 @@ const Home = (props: HomeProps) => {
     return (
         <ScrollLayout>
             <View>
-                <Text>
-                    Hom Screen
-                </Text>
-
                 <Button title={"onClick"} onPress={() => {
                     // Modal Screen
                     navigation.navigate("Modal")
                 }}/>
-
-                <VictoryBar/>
-
-                {/*<LineChart data={{dataSets: [{label: "demo", values: [{y: 1}, {y: 2}, {y: 1}]}]}}*/}
-                {/*/>*/}
-
+                <BarChart/>
             </View>
         </ScrollLayout>
     )
---
+++ src/shared/chart/bar-chart.tsx
@@ -0,0 +1,63 @@
+import React, {memo, useEffect, useState} from 'react'
+import {Bar, VictoryBar, VictoryChart, VictoryTheme, VictoryTooltip, VictoryAxis,} from "victory-native";
+
+type BarChartProps = {}
+
+type BarChartState = {
+    tickFormat: any[]
+    tickValue: any[]
+}
+
+const data = [
+    {quarter: 1, earnings: 13000, label: "Quarter 1"},
+    {quarter: 2, earnings: 16500, label: "Quarter 2"},
+    {quarter: 3, earnings: 14250, label: "Quarter 3"},
+    {quarter: 4, earnings: 19000, label: "Quarter 4"},
+];
+
+const BarChart = (props: BarChartProps) => {
+
+    const [state, setState] = useState<BarChartState>({
+        tickFormat: [],
+        tickValue: []
+    })
+
+    useEffect(() => {
+
+        const tickFormat = data.map(payload => payload.label)
+
+        const tickValue = data.map(payload => payload.quarter)
+
+        const newState: BarChartState = {
+            tickValue,
+            tickFormat
+        }
+
+        setState(newState)
+
+    }, [])
+
+    if (Object.keys(state).length == 0) return null
+
+    return (
+        <VictoryChart
+            theme={VictoryTheme.material}
+            domainPadding={20}>
+            <VictoryAxis tickValues={state.tickValue}
+                         tickFormat={state.tickFormat}/>
+            <VictoryAxis
+                dependentAxis={true}
+                tickFormat={(x) => (`$${x / 1000}k`)}
+            />
+            <VictoryBar
+                labelComponent={<VictoryTooltip
+                    renderInPortal={false}/>}
+                data={data}
+                x="quarter"
+                y="earnings"
+            />
+        </VictoryChart>
+    )
+}
+
+export default memo(BarChart)
