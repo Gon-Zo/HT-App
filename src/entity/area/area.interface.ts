ea1023d7a4051b093ddb03e5fa0cb645647c509a
‘gonzo’ <pnw123456@icloud.com>
2021-06-17 21:35:51 +0900

    refactor: area interface change
    - 주석 제거

--- src/entity/area/area.interface.ts
+++ src/entity/area/area.interface.ts
@@ -7,17 +7,12 @@ export type StackParamList = {
         key: string
     },
     AreaDetail: {
-        title : string
+        title: string
     }
 }
 
-// export interface IAreaDetailProps {
-//     navigation : NavigationProp<StackParamList , 'AreaDetail'>,
-//     route: RouteProp<StackParamList, 'AreaDetail'>
-// }
-
 export interface IAreaProps {
-    navigation: NavigationProp<StackParamList , 'Area'>
+    navigation: NavigationProp<StackParamList, 'Area'>
 }
 
 export interface IAreaRegionProps {
@@ -25,34 +20,8 @@ export interface IAreaRegionProps {
     route: RouteProp<StackParamList, 'AreaRegion'>
 }
 
-// export interface IAreaItemProps {
-//     item: IAreaData
-//     index: number
-//     onSwitch: Function
-// }
-//
-// export interface IAreaSubItemProps {
-//     item: IAreaSubData,
-//     index: number,
-//     onPress: Function
-// }
-
-// export interface IAreaData {
-//     key: string,
-//     active: boolean,
-//     list: string[]
-// }
-
-// export interface IAreaSubData {
-//     key: string
-// }
-
 export interface IChartComponentStyledProps {
     height: number,
     width: number
 }
 
-// export interface IAreaChartComponentProps {
-//     text: string,
-//     open: boolean
-// }
