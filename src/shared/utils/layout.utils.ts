60442d9af5e0298654afc173c2ff69da6ad49a1a
gonzo <m165252@daum.net>
2021-06-19 07:41:08 +0900

    refactor: font-utilsts
    - remove font-utils.ts

--- src/shared/utils/layout.utils.ts
+++ src/shared/utils/layout.utils.ts
@@ -1,17 +1,36 @@
 import {getStatusBarHeight} from "react-native-status-bar-height";
 import {isIphoneX, getBottomSpace} from "react-native-iphone-x-helper";
-import {Dimensions} from "react-native";
-
+import { Dimensions, Platform, PixelRatio } from 'react-native';
 const status = getStatusBarHeight(true);
+
+const {
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+} = Dimensions.get('window');
 
 /**
  * current device ios 11
  */
 export const getHeight = () => {
     if (isIphoneX()) {
-        return Dimensions.get("window").height - status - getBottomSpace();
+        return SCREEN_HEIGHT - status - getBottomSpace();
     } else {
 
-        return Dimensions.get("window").height - status;
+        return SCREEN_HEIGHT - status;
     }
+};
+
+// based on iphone 5s's scale
+const scale = SCREEN_WIDTH / 320;
+
+export const actuatedNormalize = (size: number) => {
+
+    const newSize = size * scale;
+
+    if (Platform.OS === 'ios') {
+        return Math.round(PixelRatio.roundToNearestPixel(newSize));
+    } else {
+        return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+    }
+
 };
