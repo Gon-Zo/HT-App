4453f6aca77ea5fbee59f39df90d464cfbcec22d
‘gonzo’ <pnw123456@icloud.com>
2021-06-22 22:52:46 +0900

    style: header style update
    -header style to size down

--- src/shared/utils/layout.utils.ts
+++ src/shared/utils/layout.utils.ts
@@ -38,8 +38,8 @@ export const actuatedNormalize = (size: number) => {
 
 const HEADER_TITLE_STYLE: Animated.WithAnimatedValue<StyleProp<TextStyle>> = {
     fontFamily: "NanumSquare_acB",
-    fontWeight: '800',
-    fontSize: 20
+    fontWeight: '600',
+    fontSize: 18
 }
 
 export const SCREEN_OPTION = {
