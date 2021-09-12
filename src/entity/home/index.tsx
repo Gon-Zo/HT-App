1a0dbfc0c582caeb72784b8bd5d37b0433cd5194
‘gonzo’ <pnw123456@icloud.com>
2021-09-12 12:17:48 +0900

    feat: chart develop
    - e-chart wrap add
    - web view add

--- src/entity/home/index.tsx
+++ src/entity/home/index.tsx
@@ -1,14 +1,18 @@
-import React, {memo, useEffect} from 'react'
+import React, { memo, useEffect } from 'react'
 import ScrollLayout from "../../shared/component/scroll-layout";
-import {HomeProps} from "./home.interface";
+import { HomeProps } from "./home.interface";
 import BarChart from "../../shared/chart/bar-chart";
-import {AppText, LogoComponent, SearchUiButton} from "../../shared/component/component";
-import {shallowEqual, useDispatch, useSelector} from "react-redux";
-import {getRealEstateList} from "./home.reducer";
-import {IRootState} from "../../shared/reducer";
-import {Text, TouchableOpacity, View} from "react-native";
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
-import {HomeBarChart} from "./home.component";
+import { AppText, LogoComponent, SearchUiButton } from "../../shared/component/component";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { getRealEstateList } from "./home.reducer";
+import { IRootState } from "../../shared/reducer";
+import { Text, TouchableOpacity, View } from "react-native";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { HomeBarChart } from "./home.component";
+
+
+// @ts-ignore
+import { ECharts } from "react-native-echarts-wrapper";
 
 const Home = (props: HomeProps) => {
 
@@ -31,19 +35,43 @@ const Home = (props: HomeProps) => {
         navigation.navigate("Modal")
     }
 
+    const options = {
+        xAxis: {
+            type: "category",
+            data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        },
+        yAxis: {
+            type: "value"
+        },
+        series: [
+            {
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: "line"
+            }
+        ]
+    }
     return (
         <ScrollLayout stickyList={[1]}>
             <LogoComponent/>
             <SearchUiButton onPress={goSearch}/>
-            <View>
-                <Text>토지거래</Text>
-                <Text>순수토지거래</Text>
-                <Text>거축물거래</Text>
-                <Text>주택거래</Text>
-                <Text>아프트거래</Text>
-                <Text>주택매매거래</Text>
-                <Text>아파트매매거래</Text>
+
+            <View style={{flex: 1}}>
+                <ECharts
+                    option={options}
+                    backgroundColor="rgba(93, 169, 81, 0.3)"
+                />
             </View>
+
+            {/*<View>*/}
+            {/*    <Text>토지거래</Text>*/}
+            {/*    <Text>순수토지거래</Text>*/}
+            {/*    <Text>거축물거래</Text>*/}
+            {/*    <Text>주택거래</Text>*/}
+            {/*    <Text>아프트거래</Text>*/}
+            {/*    <Text>주택매매거래</Text>*/}
+            {/*    <Text>아파트매매거래</Text>*/}
+            {/*</View>*/}
+
             {/*<HomeBarChart data={realEstateListData} load={realEstateListLoad}/>*/}
 
         </ScrollLayout>
