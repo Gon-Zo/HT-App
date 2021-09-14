5de786dd1d31501357808fe18dbf29e3283bf97a
‘gonzo’ <pnw123456@icloud.com>
2021-09-14 19:04:59 +0900

    build: yarn update
    - chart package remove
    - chart component remove

--- src/entity/home/index.tsx
+++ src/entity/home/index.tsx
@@ -1,18 +1,20 @@
-import React, { memo, useEffect } from 'react'
+import React, { memo, useEffect, useRef } from 'react'
 import ScrollLayout from "../../shared/component/scroll-layout";
 import { HomeProps } from "./home.interface";
-import BarChart from "../../shared/chart/bar-chart";
 import { AppText, LogoComponent, SearchUiButton } from "../../shared/component/component";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import { getRealEstateList } from "./home.reducer";
 import { IRootState } from "../../shared/reducer";
-import { Text, TouchableOpacity, View } from "react-native";
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { HomeBarChart } from "./home.component";
 
 
-// @ts-ignore
-import { ECharts } from "react-native-echarts-wrapper";
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
+import { View, Text, Dimensions } from "react-native";
 
 const Home = (props: HomeProps) => {
 
@@ -35,48 +37,60 @@ const Home = (props: HomeProps) => {
         navigation.navigate("Modal")
     }
 
-    const options = {
-        xAxis: {
-            type: "category",
-            data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-        },
-        yAxis: {
-            type: "value"
-        },
-        series: [
-            {
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: "line"
-            }
-        ]
-    }
     return (
         <ScrollLayout stickyList={[1]}>
             <LogoComponent/>
             <SearchUiButton onPress={goSearch}/>
 
-            <View style={{flex: 1}}>
-                <ECharts
-                    option={options}
-                    backgroundColor="rgba(93, 169, 81, 0.3)"
+            <View>
+                <Text>Bezier Line Chart</Text>
+                <LineChart
+                    data={{
+                        labels: ["January", "February", "March", "April", "May", "June"],
+                        datasets: [
+                            {
+                                data: [
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100
+                                ]
+                            }
+                        ]
+                    }}
+                    width={Dimensions.get("window").width} // from react-native
+                    height={220}
+                    yAxisLabel="$"
+                    yAxisSuffix="k"
+                    yAxisInterval={1} // optional, defaults to 1
+                    chartConfig={{
+                        backgroundColor: "#e26a00",
+                        backgroundGradientFrom: "#fb8c00",
+                        backgroundGradientTo: "#ffa726",
+                        decimalPlaces: 2, // optional, defaults to 2dp
+                        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        style: {
+                            borderRadius: 16
+                        },
+                        propsForDots: {
+                            r: "6",
+                            strokeWidth: "2",
+                            stroke: "#ffa726"
+                        }
+                    }}
+                    bezier
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16
+                    }}
                 />
             </View>
 
-            {/*<View>*/}
-            {/*    <Text>토지거래</Text>*/}
-            {/*    <Text>순수토지거래</Text>*/}
-            {/*    <Text>거축물거래</Text>*/}
-            {/*    <Text>주택거래</Text>*/}
-            {/*    <Text>아프트거래</Text>*/}
-            {/*    <Text>주택매매거래</Text>*/}
-            {/*    <Text>아파트매매거래</Text>*/}
-            {/*</View>*/}
-
-            {/*<HomeBarChart data={realEstateListData} load={realEstateListLoad}/>*/}
-
         </ScrollLayout>
     )
-
 }
 
 export default memo(Home);
