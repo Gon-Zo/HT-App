53a49285c925156684b8b5c10ed7cf78127bfa13
‘gonzo’ <pnw123456@icloud.com>
2021-10-03 12:54:34 +0900

    feat: datepicker develop

--- src/entity/dashboard/dashboard-detail.tsx
+++ src/entity/dashboard/dashboard-detail.tsx
@@ -1,12 +1,16 @@
-import React, {useEffect} from "react";
-import {IDashboardDetailProps} from "./dashboard.interface";
-import {BackButton} from "../../shared/component/component";
+import React, { useEffect, useState } from "react";
+import { IDashboardDetailProps } from "./dashboard.interface";
+import { BackButton } from "../../shared/component/component";
+import { Button, View } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const DashboardDetail = (props: IDashboardDetailProps) => {
 
     const {navigation, route} = props
 
     const {key, index, code} = route.params
+
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
     useEffect(() => {
 
@@ -17,8 +21,31 @@ const DashboardDetail = (props: IDashboardDetailProps) => {
 
     }, [])
 
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = (date: any) => {
+        console.warn("A date has been picked: ", date);
+        hideDatePicker();
+    };
+
     return (
         <React.Fragment>
+
+            <View>
+                <Button title="Show Date Picker" onPress={showDatePicker}/>
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible}
+                    mode="date"
+                    onConfirm={handleConfirm}
+                    onCancel={hideDatePicker}
+                />
+            </View>
 
         </React.Fragment>
     )
