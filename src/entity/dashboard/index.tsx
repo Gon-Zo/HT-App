e957990a64b7aacdc8701c03268f39f2676277e7
‘gonzo’ <pnw123456@icloud.com>
2021-10-05 22:28:19 +0900

    feat: dashboard layout develop

--- src/entity/dashboard/index.tsx
+++ src/entity/dashboard/index.tsx
@@ -3,7 +3,7 @@ import { IDashboardProps } from "./dashboard.interface";
 import { DashBoardSafeAreaView } from "./dashboard.style";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const Dashboard = (props: IDashboardProps) => {
 
@@ -14,12 +14,9 @@ const Dashboard = (props: IDashboardProps) => {
     useEffect(() => {
         navigation.setOptions({
             headerTitle: "대시보드",
+            headerShown: false
         })
     }, [])
-
-    const onPress = (select: any) => {
-        navigation.navigate("DashboardDetail", select)
-    }
 
     const goFilter = () => {
         navigation.navigate("Filter")
@@ -40,11 +37,9 @@ const Dashboard = (props: IDashboardProps) => {
                 </TouchableOpacity>
             </View>
 
-            <ScrollView style={
-                {
-                    flex : 1
-                }
-            }>
+            <ScrollView style={{
+                flex: 1
+            }}>
 
                 <View style={{
                     padding: 10
@@ -59,36 +54,89 @@ const Dashboard = (props: IDashboardProps) => {
                     </Text>
                 </View>
 
-                <View style={
-                    {
-                        backgroundColor: "#f00",
-                        flexDirection : "column"
-                    }
-                }>
+                {/*<View style={*/}
+                {/*    {*/}
+                {/*        backgroundColor: "#f00",*/}
+                {/*        flexDirection : "column"*/}
+                {/*    }*/}
+                {/*}>*/}
 
-                    <Text>
-                        2020.02.01 ~ 2020.03.01
-                    </Text>
+                {/*    <Text>*/}
+                {/*        2020.02.01 ~ 2020.03.01*/}
+                {/*    </Text>*/}
 
-                    <Text>
-                        토지 거래
-                    </Text>
+                {/*    <Text>*/}
+                {/*        토지 거래*/}
+                {/*    </Text>*/}
 
-                </View>
+                {/*</View>*/}
 
-                <View>
-                    <View>
-                        <Text>TEST</Text>
-                    </View>
-                    <View>
-                        <Text>TEST</Text>
-                    </View>
-                </View>
+                <CardGroup/>
 
             </ScrollView>
 
         </DashBoardSafeAreaView>
     )
 }
+
+const CardGroup = (props: any) => {
+    return (
+        <View style={
+            {
+                marginTop : 10
+            }
+        }>
+            <View>
+                <Text>아파트 전월세</Text>
+            </View>
+            
+            <View style={{
+                flexDirection: "row",
+                height: 150,
+            }}>
+                <View style={
+                    [{
+                        backgroundColor: "tomato",
+                    }, styles.cardWrap]
+                }>
+                    <Text>신동아블루아광화문의 꿈</Text>
+                    <Text>월세: 300</Text>
+                    <Text>보증금: 60,000</Text>
+                </View>
+                <View style={
+                    [
+                        {
+                            backgroundColor: "orange"
+                        },
+                        styles.cardWrap
+                    ]
+                }>
+                    <Text>광화문풍림스페이스본(101동~105동)</Text>
+                    <Text>월세: 300</Text>
+                    <Text>보증금: 60,000</Text>
+                </View>
+                <View style={[
+                    {
+                        backgroundColor: "gold",
+                    },
+                    styles.cardWrap
+                ]}>
+                    <Text>광화문풍림스페이스본(101동~105동)</Text>
+                    <Text>월세: 300</Text>
+                    <Text>보증금: 60,000</Text>
+                </View>
+            </View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    cardWrap: {
+        flex: 1,
+        margin: 5,
+        borderRadius: 8
+    }
+})
 
 export default Dashboard
