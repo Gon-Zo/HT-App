27f525eb37e6f7b9d9fb809066b45d0264e4a905
‘gonzo’ <pnw123456@icloud.com>
2021-10-05 21:45:56 +0900

    feat: dashboard view create

--- src/entity/dashboard/index.tsx
+++ src/entity/dashboard/index.tsx
@@ -3,7 +3,7 @@ import { IDashboardProps } from "./dashboard.interface";
 import { DashBoardSafeAreaView } from "./dashboard.style";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 const Dashboard = (props: IDashboardProps) => {
 
@@ -32,13 +32,60 @@ const Dashboard = (props: IDashboardProps) => {
                 style={
                     {
                         alignItems: "flex-end",
-                        paddingRight: 10
+                        paddingRight: 10,
                     }
                 }>
                 <TouchableOpacity onPress={goFilter}>
                     <FontAwesomeIcon icon={['fas', 'filter']} size={17} color={'#000'}/>
                 </TouchableOpacity>
             </View>
+
+            <ScrollView style={
+                {
+                    flex : 1
+                }
+            }>
+
+                <View style={{
+                    padding: 10
+                }}>
+                    <Text style={
+                        {
+                            fontSize: 27,
+                            fontWeight: "bold",
+                        }
+                    }>
+                        서울시
+                    </Text>
+                </View>
+
+                <View style={
+                    {
+                        backgroundColor: "#f00",
+                        flexDirection : "column"
+                    }
+                }>
+
+                    <Text>
+                        2020.02.01 ~ 2020.03.01
+                    </Text>
+
+                    <Text>
+                        토지 거래
+                    </Text>
+
+                </View>
+
+                <View>
+                    <View>
+                        <Text>TEST</Text>
+                    </View>
+                    <View>
+                        <Text>TEST</Text>
+                    </View>
+                </View>
+
+            </ScrollView>
 
         </DashBoardSafeAreaView>
     )
---
+++ src/shared/utils/color-utils.ts
@@ -0,0 +1,2 @@
+// @ts-ignore
+const theme = ["tomato", "orange", "gold", "cyan", "navy"]
