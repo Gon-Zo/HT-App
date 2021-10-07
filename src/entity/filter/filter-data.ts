3ec3b0dca8da07756efb4a7d074ee11f8e9f4441
‘gonzo’ <pnw123456@icloud.com>
2021-10-07 21:07:38 +0900

    feat: filter view tag select develop

---
+++ src/entity/filter/filter-data.ts
@@ -0,0 +1,37 @@
+export const arrOfPicker = [
+    {
+        id : 1,
+        label: '토지 거래',
+        value: '01'
+    },
+    {
+        id : 2,
+        label: '순수 토지 거래',
+        value: '02'
+    },
+    {
+        id : 3,
+        label: '건축물 거래',
+        value: '03'
+    },
+    {
+        id : 4,
+        label: '주택 거래',
+        value: '04'
+    },
+    {
+        id : 5,
+        label: '아파트 거래',
+        value: '05'
+    },
+    {
+        id : 6,
+        label: '주택 매매 거래',
+        value: '06'
+    },
+    {
+        id : 7,
+        label: '아파트 매매 거래',
+        value: '07'
+    },
+]
--- src/entity/filter/index.tsx
+++ src/entity/filter/index.tsx
@@ -1,25 +1,109 @@
-import React from 'react'
-import { Text, View, SafeAreaView } from "react-native";
-import { BackButton } from "../../shared/component/component";
+import React, { useState , useRef } from 'react'
+import { Text, View, SafeAreaView, StyleSheet, Alert } from "react-native";
+// @ts-ignore
+import { TagSelect } from 'react-native-tag-select';
+import { arrOfPicker } from "./filter-data";
+import { Button } from "react-native-elements";
+
+type FilterState = {
+    transactionType: string,
+    tag : any
+}
 
 const Filter = (props: any) => {
 
     const {navigation} = props
 
+    const tagRef = useRef()
+
+    const [state, setState] = useState<FilterState>({
+        transactionType: '01',
+        tag : null
+    })
+
+    const toValueChange = (itemValue: any, itemIndex: any) => {
+        const newState: FilterState = {
+            ...state,
+            transactionType: itemValue
+        }
+        setState(newState)
+    }
+
     return (
-        <SafeAreaView style={{
-            flex: 1,
-            backgroundColor: "#fff",
-            flexDirection: "column"
-        }}>
-            <BackButton navigation={navigation}/>
-            <View>
-                <Text>
-                    TEST
-                </Text>
+        <SafeAreaView style={styles.viewWrap}>
+
+            <View style={{flex: 1, backgroundColor: "darkorange"}}>
+
             </View>
+
+            <View style={{flex: .5}}>
+
+                <Text style={{fontSize: 25, fontWeight: "bold", marginLeft: 10}}>
+                    거래 유형
+                </Text>
+
+                <View style={{
+                    flexDirection : 'column'
+                }}>
+
+                    <TagSelect
+                        data={arrOfPicker}
+                        max={1}
+                        ref={tagRef}
+                        itemStyle={styles.item}
+                        itemLabelStyle={styles.label}
+                        itemStyleSelected={styles.itemSelected}
+                        itemLabelStyleSelected={styles.labelSelected}
+                    />
+
+                </View>
+
+            </View>
+
+            <View style={{flex: 1, backgroundColor: "green"}}>
+                <Button title={"test"} onPress={()=>{
+                    // @ts-ignore
+                    console.log(">>>>>>>>>>." , tagRef.current.itemsSelected)
+                }}/>
+            </View>
+
+            {/*<View style={{flex: .17, backgroundColor: "red"}}>*/}
+            {/*    <Button title="적용"/>*/}
+            {/*</View>*/}
         </SafeAreaView>
     )
+
 }
+
+const styles = StyleSheet.create({
+    viewWrap: {
+        flex: 1,
+        backgroundColor: "tomato",
+        flexDirection: "column"
+    },
+    viewMargin: {
+        margin: 10
+    },
+    labelText: {
+        color: '#333',
+        fontSize: 15,
+        fontWeight: '500',
+        marginBottom: 15,
+    },
+    item: {
+        borderWidth: 1,
+        borderColor: '#333',
+        backgroundColor: '#FFF',
+    },
+    label: {
+        color: '#333'
+    },
+    itemSelected: {
+        backgroundColor: '#333',
+    },
+    labelSelected: {
+        color: '#FFF',
+    },
+})
 
 export default React.memo(Filter)
