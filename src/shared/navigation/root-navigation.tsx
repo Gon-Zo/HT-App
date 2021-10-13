02c6016a912ff744724ec6615bba97862d749d9d
‘gonzo’ <pnw123456@icloud.com>
2021-10-13 22:14:27 +0900

    chore: row dispatch add

--- src/shared/navigation/root-navigation.tsx
+++ src/shared/navigation/root-navigation.tsx
@@ -1,13 +1,23 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import TabNavigation from "./tab-navigation";
 import Search from "../../entity/search";
 import Filter from "../../entity/filter";
+import { useDispatch } from "react-redux";
+import { getAreaCodes } from "../../entity/app-shared/app-shared.reducer";
 
 const RootStack = createStackNavigator()
 
 const RootStackNavigation = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        // todo : 나중에 지울것
+        dispatch(getAreaCodes())
+    }, [])
+
     return (
         <NavigationContainer>
             <RootStack.Navigator mode={"modal"} headerMode={"none"}>
