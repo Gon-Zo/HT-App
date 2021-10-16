28fa345917a55afc14b37ddd2efdc059f79015b7
‘gonzo’ <pnw123456@icloud.com>
2021-10-16 11:32:12 +0900

    chore: remove dashboard detail view

--- src/shared/navigation/dashboard-navigation.tsx
+++ src/shared/navigation/dashboard-navigation.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import {createStackNavigator} from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack'
 import Dashboard from "../../entity/dashboard";
-import DashboardDetail from "../../entity/dashboard/dashboard-detail";
-import {SCREEN_OPTION} from "../utils/layout.utils";
+import { SCREEN_OPTION } from "../utils/layout.utils";
 
 const DashboardStack = createStackNavigator()
 
@@ -10,7 +9,6 @@ const DashboardNavigation = (props: any) => {
     return (
         <DashboardStack.Navigator screenOptions={SCREEN_OPTION}>
             <DashboardStack.Screen name={"Dashboard"} component={Dashboard}/>
-            <DashboardStack.Screen name={"DashboardDetail"} component={DashboardDetail}/>
         </DashboardStack.Navigator>
     )
 }
--- src/shared/navigation/root-navigation.tsx
+++ src/shared/navigation/root-navigation.tsx
@@ -2,7 +2,6 @@ import React, { memo, useEffect } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import TabNavigation from "./tab-navigation";
-import Search from "../../entity/search";
 import Filter from "../../entity/filter";
 
 const RootStack = createStackNavigator()
@@ -16,7 +15,6 @@ const RootStackNavigation = () => {
         <NavigationContainer>
             <RootStack.Navigator mode={"modal"} headerMode={"none"}>
                 <RootStack.Screen name={"Tap"} component={TabNavigation}/>
-                <RootStack.Screen name={"Modal"} component={Search}/>
                 <RootStack.Screen name={"Filter"} component={Filter}/>
             </RootStack.Navigator>
         </NavigationContainer>
